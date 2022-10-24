1d226210504379ee030cd1139c44e0c47293edb3
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-10-24 10:56:47 +0200

    Users array extracted to data.js

--- app.js
+++ app.js
@@ -5,50 +5,7 @@ const port = 3000;
 // Your github page origin has to be written EXACTLY like this! https://behu-kea.github.io
 const URL_FOR_FRONTEND = "YOUR_GITHUB_PAGE_ORIGIN_HERE";
 
-let users = [
-    {
-        id: 1,
-        name: "Birgitte Kirk Iversen",
-        mail: "bki@mail.dk",
-        title: "Senior Lecturer",
-        image: "https://www.baaa.dk/media/u4gorzsd/birgitte-kirk-iversen2.jpg?anchor=center&mode=crop&width=800&height=450&rnd=132792921541630000&format=webp"
-    },
-    {
-        id: 4,
-        name: "Rasmus Cederdorff",
-        mail: "race@mail.dk",
-        title: "Senior Lecturer",
-        image: "https://www.baaa.dk/media/devlvvgj/rasmus-cederdorff.jpg?anchor=center&mode=crop&width=800&height=450&rnd=132792921695570000&format=webp"
-    },
-    {
-        id: 5,
-        name: "Dan Okkels Brendstrup",
-        mail: "dob@mail.dk",
-        title: "Lecturer",
-        image: "https://www.eaaa.dk/media/bdojel41/dan-okkels-brendstrup.jpg?anchor=center&mode=crop&width=800&height=450&rnd=132792921559630000&format=webp"
-    },
-    {
-        id: 6,
-        name: "Kasper Fischer Topp",
-        mail: "kato@mail.dk",
-        title: "Lecturer",
-        image: "https://www.eaaa.dk/media/lxzcybme/kasper-topp.jpg?anchor=center&mode=crop&width=800&height=450&rnd=132792921618200000&format=webp"
-    },
-    {
-        id: 7,
-        name: "Line Skjødt",
-        mail: "lskj@mail.dk",
-        title: "Senior Lecturer & Internship Coordinator",
-        image: "https://www.eaaa.dk/media/14qpfeq4/line-skj%C3%B8dt.jpg?anchor=center&mode=crop&width=800&height=450&rnd=132792921638700000&format=webp"
-    },
-    {
-        id: 8,
-        name: "Martin Aagaard Nøhr",
-        mail: "mnor@mail.dk",
-        title: "Lecturer",
-        image: "https://www.eaaa.dk/media/oayjq02h/martin-n%C3%B8hr.jpg?anchor=center&mode=crop&width=800&height=450&rnd=132792921658800000&format=webp"
-    }
-];
+const users = require("./data"); // users array imported from data.js
 
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
@@ -56,10 +13,12 @@ app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 // If the application is running localhost allow all requests,
 // otherwise add cors for specific website
 // Remember to add the NODE_ENV="prod" on server!
-const cors_url = process.env.NODE_ENV === 'prod' ? URL_FOR_FRONTEND: "*";
-app.use(cors({
-    origin: cors_url
-}));
+const cors_url = process.env.NODE_ENV === "prod" ? URL_FOR_FRONTEND : "*";
+app.use(
+    cors({
+        origin: cors_url
+    })
+);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
