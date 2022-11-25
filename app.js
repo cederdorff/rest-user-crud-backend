8df03cca8dde54bac96c53e6326060a3ea718225
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-11-25 17:36:17 +0100

    from js array to mysql db

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv');
+require("dotenv");
 const mysqlConnection = require("./database");
 const port = 3000;
 
 // Your github page origin has to be written EXACTLY like this! https://behu-kea.github.io
 const URL_FOR_FRONTEND = "YOUR_GITHUB_PAGE_ORIGIN_HERE";
-
-let users = require("./data"); // users array imported from data.js
 
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
@@ -23,125 +21,76 @@ app.use(
     })
 );
 
-app.get('/api/wishes', (req, res) => {
-    const query = "SELECT * FROM wishlist.wish;";
-    mysqlConnection.query(
-        query,
-        (err, results, fields) => {
-            if (!err) {
-                res.json(results);
-            } else {
-                console.log(err);
-            }
+// get all users from database
+app.get("/users", (request, response) => {
+    const query = "SELECT * FROM users;";
+    mysqlConnection.query(query, (error, results, fields) => {
+        if (error) {
+            console.log(error);
+        } else {
+            response.json(results);
         }
-    );
-})
+    });
+});
+
+// get one user from database by given id
+app.get("/users/:id", (request, response) => {
+    const id = request.params.id;
+    const query = `SELECT * FROM users WHERE id=${id};`;
+
+    mysqlConnection.query(query, (error, results, fields) => {
+        console.log(results);
+        if (error) {
+            console.log(error);
+        } else {
+            response.json(results[0]);
+        }
+    });
+});
 
 // create user
-app.post('/api/wishes', (req, res) => {
-        const text = req.body.text;
-        const price = req.body.price;
-        const description = req.body.description;
+app.post("/users", (request, response) => {
+    const newUser = request.body;
+    const query = `INSERT INTO users(name, mail, title, image) values("${newUser.name}", "${newUser.mail}", "${newUser.title}", "${newUser.image}");`;
 
-        mysqlConnection.query('INSERT INTO wish(text, price, description) values (?, ?, ?)', [text, price, description],
-            (err, results, fields) => {
-                if (!err) {
-                    res.json('success');
-                } else {
-                    console.log(err);
-                    res.json('error');
-                }
-            }
-        )
-
-    }
-);
-
-// This is the correct way to handle user input
-app.get('/database-user-input', (req, res) => {
-    const query = "SELECT * FROM wishlist.wish where description = ?;";
-    const description = `asd`;
-    mysqlConnection.query(
-        query,
-        [description],
-        (err, results, fields) => {
-            if (!err) {
-                res.json(results);
-            } else {
-                console.log(err);
-            }
+    mysqlConnection.query(query, (error, results, fields) => {
+        if (error) {
+            console.log(error);
+        } else {
+            response.json(results);
         }
-    );
-})
-
-// THE FOLLOWING CODE IS VULNERABLE AND SHOULD NOT BE USED!!!
-// IT DELETES THE USERS TABLE IF YOU HAVE A TABLE CALLED USERS IN YOUR DATABASE
-// To run this endpoint with sql injection go to the following page:
-// http://localhost:3000/database-sql-injection/'; DROP TABLE users;--
-// Read more about it here: https://planetscale.com/blog/how-to-prevent-sql-injection-attacks-in-node-js
-app.get('/database-sql-injection/:description', (req, res) => {
-    // THIS WILL DO AN SQL INJECTION: VERY BAD!!!!!
-    const hackQuery = `SELECT * FROM wishlist.wish where description = '${req.params.description}';`;
-    mysqlConnection.query(
-        hackQuery,
-        (err, results, fields) => {
-            if (!err) {
-                res.json(results);
-            } else {
-                console.log(err);
-            }
-        }
-    );
-})
-
-// DO NOT MAKE YOUR CODE VULNERABLE TO SQL INJECTION!!!
-
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-// READ: read all users from users
-app.get("/users", (req, res) => {
-    return res.json(users);
-});
-
-// READ: get user by id
-app.get("/users/:id", (req, res) => {
-    const id = req.params.id;
-    const user = users.find(item => item.id == id);
-    return res.json(user);
-});
-
-// CREATE: create new user and add to users
-app.post("/users", (req, res) => {
-    let newUser = req.body;
-    const timestamp = Date.now(); // dummy generated user id
-    newUser.id = timestamp;
-    users.push(newUser);
-    return res.json(users);
+    });
 });
 
 // UPDATE: update existing user
-app.put("/users/:id", (req, res) => {
-    const id = req.params.id;
-    const userData = req.body;
-    let user = users.find(item => item.id == id);
-    user.name = userData.name;
-    user.title = userData.title;
-    user.mail = userData.mail;
-    user.image = userData.image;
-    return res.json(users);
+app.put("/users/:id", (request, response) => {
+    const id = request.params.id;
+    const user = request.body;
+    const query = `UPDATE users SET name="${user.name}", mail="${user.mail}", title="${user.title}", image="${user.image}" WHERE id=${id};`;
+
+    mysqlConnection.query(query, (error, results, fields) => {
+        if (error) {
+            console.log(error);
+        } else {
+            response.json(results);
+        }
+    });
 });
 
 // DELETE: delete user
-app.delete("/users/:id", (req, res) => {
-    const id = req.params.id;
-    users = users.filter(item => item.id != id);
-    return res.json(users);
+app.delete("/users/:id", (request, response) => {
+    const id = request.params.id;
+    const query = `DELETE FROM users WHERE id=${id};`;
+
+    mysqlConnection.query(query, (error, results, fields) => {
+        if (error) {
+            console.log(error);
+        } else {
+            response.json(results);
+        }
+    });
 });
 
 app.listen(port, () => {
     console.log(`Node.js REST API listening at http://localhost:${port}`);
 });
-
--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 // Read the environment variables
-require('dotenv').config();
+require("dotenv").config();
 
 const mysqlConnection = mysql.createConnection({
     host: process.env.HOST,
@@ -8,15 +8,15 @@ const mysqlConnection = mysql.createConnection({
     user: process.env.MYSQL_USER,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
-    multipleStatements: true,
+    multipleStatements: true
 });
 
-mysqlConnection.connect((err) => {
-    if (!err) {
-        console.log("Connected");
-    } else {
+mysqlConnection.connect(error => {
+    if (error) {
         console.log(err);
         console.log("Connection Failed");
+    } else {
+        console.log("Connected");
     }
 });
 
